3861a819025f39aa62ecd4b8ec7d67b31a29f6ba
Iman Tumorang <bxcodec@gmail.com>
2018-03-29 07:32:19 +0700

    update readme

--- og_test.go
+++ og_test.go
@@ -11,11 +11,7 @@ import (
 const (
 	mockHtml = `
 	<!DOCTYPE html>
-	<!--[if (gt IE 9)|!(IE)]> <!--> <html lang="en" class="no-js section-technology format-extra-long tone-feature app-article page-theme-standard has-comments has-cover-media no-ribbon type-size-small" itemid="https://www.nytimes.com/2018/03/21/technology/facebook-zucktown-willow-village.html" itemtype="http://schema.org/NewsArticle"  itemscope xmlns:og="http://opengraphprotocol.org/schema/"> <!--<![endif]-->
-	<!--[if IE 9]> <html lang="en" class="no-js ie9 lt-ie10 section-technology format-extra-long tone-feature app-article page-theme-standard has-comments has-cover-media no-ribbon type-size-small" xmlns:og="http://opengraphprotocol.org/schema/"> <![endif]-->
-	<!--[if IE 8]> <html lang="en" class="no-js ie8 lt-ie10 lt-ie9 section-technology format-extra-long tone-feature app-article page-theme-standard has-comments has-cover-media no-ribbon type-size-small" xmlns:og="http://opengraphprotocol.org/schema/"> <![endif]-->
-	<!--[if (lt IE 8)]> <html lang="en" class="no-js lt-ie10 lt-ie9 lt-ie8 section-technology format-extra-long tone-feature app-article page-theme-standard has-comments has-cover-media no-ribbon type-size-small" xmlns:og="http://opengraphprotocol.org/schema/"> <![endif]-->
-	<head>
+ 	<head>
 		<title>Welcome to Zucktown. Where Everything Is Just Zucky. - The New York Times</title>
 			<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
@@ -158,147 +154,10 @@ const (
 	<meta property="fb:pages" content="8254939527" />
 	<meta property="fb:pages" content="98934992430" />
 	<meta property="fb:pages" content="993603507345855" />
-		
-				<link id="legacy-zam5nzz" rel="stylesheet" type="text/css" href="https://typeface.nyt.com/css/zam5nzz.css" media="all" />
-		<!--[if (gt IE 9)|!(IE)]> <!-->
-			<link rel="stylesheet" type="text/css" media="screen" href="https://g1.nyt.com/assets/article/20180322-142545/css/article/story/styles.css" />
-		<!--<![endif]-->
-		<!--[if lte IE 9]>
-			<link rel="stylesheet" type="text/css" media="screen" href="https://g1.nyt.com/assets/article/20180322-142545/css/article/story/styles-ie.css" />
-		<![endif]-->
-	<link rel="stylesheet" type="text/css" media="print" href="https://g1.nyt.com/assets/article/20180322-142545/css/article/story/styles-print.css" />
-					<!--  begin abra  -->
-	<script>
-	var NYTD=NYTD||{};NYTD.Abra=function(t){"use strict";function n(t){var n=i[t];return n&&n[1]||null}function e(t,n){if(t){var e,o,r=n[0],i=n[1],u=0,c=0;if(1!==i.length||4294967296!==i[0])for(e=a(t+" "+r)>>>0,u=0,c=0;o=i[u++];)if(e<(c+=o[0]))return o}}function o(n,e,o,a){s+="subject="+n+"&test="+encodeURIComponent(e)+"&variant="+encodeURIComponent(o||0)+"&url="+encodeURIComponent(t.location.href)+"&instant=1&skipAugment=true\n",a&&f.push(a),c||(c=t.setTimeout(r,0))}function r(){var n=new t.XMLHttpRequest,e=f;n.withCredentials=!0,n.open("POST",u),n.onreadystatechange=function(){var t,o;if(4==n.readyState)for(t=200==n.status?null:new Error(n.statusText);o=e.shift();)o(t)},n.send(s),s="",f=[],c=null}function a(t){for(var n,e,o,r,a,i,u,c=0,s=0,f=[],l=[n=1732584193,e=4023233417,~n,~e,3285377520],h=[],p=t.length;s<=p;)h[s>>2]|=(s<p?t.charCodeAt(s):128)<<8*(3-s++%4);for(h[u=p+8>>2|15]=p<<3;c<=u;c+=16){for(n=l,s=0;s<80;n=[0|[(i=((t=n[0])<<5|t>>>27)+n[4]+(f[s]=s<16?~~h[c+s]:i<<1|i>>>31)+1518500249)+((e=n[1])&(o=n[2])|~e&(r=n[3])),a=i+(e^o^r)+341275144,i+(e&o|e&r|o&r)+882459459,a+1535694389][0|s++/20],t,e<<30|e>>>2,o,r])i=f[s-3]^f[s-8]^f[s-14]^f[s-16];for(s=5;s;)l[--s]=l[s]+n[s]|0}return l[0]}var i,u,c,s="",f=[];return n.init=function(n,r){var a,c,s,f,l,h=[],p=(t.document.cookie.match(/(^|;) *nyt-a=([^;]*)/)||[])[2],d=(t.document.cookie.match(/(^|;) *ab7=([^;]*)/)||[])[2],m=new RegExp("[?&]abra(=([^&#]*))"),g=m.exec(t.location.search);if(g&&g[2]&&(d=d?g[2]+"&"+d:g[2]),i)throw new Error("can't init twice");if(u=r,i={},d)for(d=decodeURIComponent(d).split("&"),a=0;a<d.length;a++)l=d[a].split("="),l[0]in i||(i[l[0]]=[,l[1],1],o("ab-alloc",l[0],l[1]),l[1]&&h.push(l[0]+"="+l[1]));for(a=0;a<n.length;a++)s=n[a],(c=s[0])in i||(f=e(p,s)||[],i[c]=f,f[1]&&h.push(c.replace(/[^\w-]/g)+"="+(""+f[1]).replace(/[^\w-]/g)),f[2]&&o("ab-alloc",c,f[1]));h.length&&t.document.documentElement.setAttribute("data-nyt-ab",h.join(" "))},n.reportExposure=function(n,e){var r=i[n];r&&r[2]?o("ab-expose",n,r[1],e):e&&t.setTimeout(function(){e(null)},0)},n}(this);
-	</script>
-	<script>NYTD.Abra.init([["www-story-reader-satisfaction",[[21474837,"Control",1],[21474836,"VariantA",1],[4252017623,null,0]]],["www-signup-favor-test",[[2147483648,"0",0],[2147483648,"1",0]]],["www-auto-newsletter",[[4294967296,"1",0]]],["www-STRIK-nl-regi",[[4294967296,"2",0]]],["abra_dfp",[[3865470567,"control",1],[429496729,"test",1]]],["EC_SampleTest",[[2147483648,"variantA",0],[2147483648,"variantB",0]]]], '//et.nytimes.com/')</script>
-	<!--  end abra  -->
-	
-	<script>
-	/* ABRA reporting for Vi rollout. Please don't hand-edit this minified snippet. <https://github.com/nytm/vi-rollout-abra-reporting-js> (6abdd06 @ Wed Feb  7 15:26:18 EST 2018) */ function reportViRolloutToABRA(t,e,o){"use strict";var i,a,n,r,s="";if(e&&(/^mobile\./.test(window.location.hostname)?(i=(document.cookie.match(/(?:^|;) *vi=([^;]*)/)||[,""])[1],a=(document.cookie.match(/(?:^|;) *nyt\.np\.vi=([^;]*)/)||[,""])[1],n=/^b1/.test(i)?"WP_ProjectVi&variant=vi":/^z1/.test(i)?"WP_ProjectVi&variant=0":/^a2/.test(i)?"WP_ProjectVi2&variant=hp-st":/^b2/.test(i)?"WP_ProjectVi2&variant=hp":/^c2/.test(i)?"WP_ProjectVi2&variant=st":/^z2/.test(i)?"WP_ProjectVi2&variant=0":/^1\|/.test(a)?"WP_ProjectVi&variant=vi":/^2\|/.test(a)?"WP_ProjectVi&variant=0":null):/^www\./.test(window.location.hostname)&&(i=(document.cookie.match(/(?:^|;) *vi_www_hp=([^;]*)/)||[,""])[1],n=/^a2/.test(i)?"WP_ProjectVi_www_hp&variant=hp-st":/^b2/.test(i)?"WP_ProjectVi_www_hp&variant=hp":/^c2/.test(i)?"WP_ProjectVi_www_hp&variant=st":/^d2/.test(i)?"WP_ProjectVi_www_hp&variant=hp-2":/^e2/.test(i)?"WP_ProjectVi_www_hp&variant=hp-serv":/^f2/.test(i)?"WP_ProjectVi_www_hp&variant=hp-orig":/^y2/.test(i)?"WP_ProjectVi_www_hp&variant=0-2":/^z2/.test(i)?"WP_ProjectVi_www_hp&variant=0":null),n&&(s+="subject=ab-alloc&test="+n+"&url="+encodeURIComponent(window.location.href)+"&instant=1&skipAugment=true\n")),o&&(/^mobile\./.test(window.location.hostname)?(i=(document.cookie.match(/(?:^|;) *vistorymobile=([^;]*)/)||[,""])[1],r=/^a/.test(i)?"WP_ProjectVi_Story_Mobile&variant=st":/^b/.test(i)?"WP_ProjectVi_Story_Mobile&variant=sth":/^c/.test(i)?"WP_ProjectVi_Story_Mobile&variant=mw":/^z/.test(i)?"WP_ProjectVi_Story_Mobile&variant=mwh":null):/^www\./.test(window.location.hostname)&&(i=(document.cookie.match(/(?:^|;) *vistory=([^;]*)/)||[,""])[1],r=/^a/.test(i)?"WP_ProjectVi_story_desktop&variant=sd":/^b/.test(i)?"WP_ProjectVi_story_desktop&variant=sdh":/^c/.test(i)?"WP_ProjectVi_story_desktop&variant=nyt5":/^z/.test(i)?"WP_ProjectVi_story_desktop&variant=nyt5h":null),r&&(s+="subject=ab-alloc&test="+r+"&url="+encodeURIComponent(window.location.href)+"&instant=1&skipAugment=true\n")),s){var _=new XMLHttpRequest;_.withCredentials=!0,_.open("POST",t),_.send(s)}}
-	</script>
-	<script>
-	  reportViRolloutToABRA(
-		'//et.nytimes.com/',
-		false,
-		true  );
-	</script>
-					<script id="page-config-data" type="text/json">
-	{"pageconfig":{"ledeMediaSize":"full_bleed","keywords":["article-extra-long","has-embedded-interactive"],"collections":{"sections":["us","realestate","technology","economy","businessday"]}}}</script>
-	<script id="display_overrides">
-				["INCLUDE_TRANSPARENT_MASTHEAD_WITH_BLACK_ICONS","HIDE_RIBBON","HIDE_TOP_AD","HIDE_NAVIGATION-EDGE","HIDE_KICKER","HIDE_STORY-META_COMMUNITY_BUTTON"]    </script>
-	
-					<script type="text/javascript">
-			(function() {
-				var s = document.getElementsByTagName("script")[0];
-				var mediaDotNet = 'https://contextual.media.net/bidexchange.js?cid=8CU2553YN&amp;https=1';
-				var indexBid = 'https://js-sec.indexww.com/ht/p/183760-203795517182556.js';
-				var timeout = 300;
-	
-				function loadScript(tagSrc) {
-					if (tagSrc.substr(0, 4) !== 'http') {
-						var isSSL = 'https:' == document.location.protocol;
-						tagSrc = (isSSL ? 'https:' : '') + tagSrc;
-					}
-					var scriptTag = document.createElement('script'),
-						placeTag = document.getElementsByTagName("script")[0];
-					scriptTag.type = 'text/javascript';
-					scriptTag.async = true;
-					scriptTag.src = tagSrc;
-					s.parentNode.insertBefore(scriptTag, s);
-				}
-	
-				function loadGPT() {
-					if (!window.advBidxc.isAdServerLoaded) {
-						loadScript('//www.googletagservices.com/tag/js/gpt.js');
-						window.advBidxc.isAdServerLoaded = true;
-					}
-				}
-	
-				window.advBidxc = window.advBidxc || {};
-				window.advBidxc.renderAd = function () {};
-				window.advBidxc.startTime = new Date().getTime();
-				window.advBidxc.customerId = "8CU2553YN"; //Media.net Customer ID
-				window.advBidxc.timeout = 300;
-				window.advBidxc.loadGPT = setTimeout(loadGPT, window.advBidxc.timeout);
-	
-				// append index
-				var a = document.createElement("script");
-				a.type = "text/javascript";
-				a.async = true;
-				a.src = indexBid;
-				s.parentNode.insertBefore(a, s);
-	
-				// append media.net
-				var b = document.createElement("script");
-				b.type = "text/javascript";
-				b.async = true;
-				b.src = mediaDotNet;
-				s.parentNode.insertBefore(b, s);
-			})();
-		</script>
-	
-		<!-- Amazon Bidder version 2 Initialization Script -->
-		<script type="text/javascript">
-			//Load the ApsTag JavaScript Library
-			!function(a9,a,p,s,t,A,g){if(a[a9])return;function q(c,r){a[a9]._Q.push([c,r])}a[a9]={init:function(){q("i",arguments)},fetchBids:function(){q("f",arguments)},setDisplayBids:function(){},targetingKeys:function(){return[]},_Q:[]};A=p.createElement(s);A.async=!0;A.src=t;g=p.getElementsByTagName(s)[0];g.parentNode.insertBefore(A,g)}("apstag",window,document,"script","//c.amazon-adsystem.com/aax2/apstag.js");
-	
-			apstag.init({
-				pubID: '3030',
-				adServer: 'googletag'
-			});
-		</script>
-	
-	
-		<script type="text/json" id="trending-link-json">
-		</script>
-	
-	<!--esi
-	<script id="user-info-data" type="application/json">
-	<esi:include src="/svc/web-products/userinfo-v3.json" />
-	</script>
-	-->
-	<script id="magnum-feature-flags" type="application/json">["limitFabrikSave","moreFollowSuggestions","unfollowComments","scoopTool","videoVHSCover","videoVHSShareTools","videoVHSLive","videoVHSNewControls","videoVHSEmbeddedOnly","allTheEmphases","freeTrial","dedupeWhatsNext","trendingPageLinks","sprinklePaidPost","headerBidder","standardizeWhatsNextCollection","onlyLayoutA","simple","simpleExtendedByline","collectionsWhatsNext","mobileMediaViewer","podcastInLede","MovieTickets","MoreInSubsectionFix","seriesIssueMarginalia","serverSideCollectionUrls","multipleBylines","fabrikWebsocketsOnly","translationLinks","papihttps","verticalFullBleed","updateRestaurantReservations","mapDining","newsletterInlineModule","PersonalizationApiUpdate","apsTagEnabled","removeInternationalEdition","headlineBalancer","clientSideABRA","newsletterTitleTracking","surveyInterstitial","removeFBMessengerPromo","removeMarginaliaAbTest","paidpostSprinklingFix","abraOverrideVersion","headlineBalancerEverywhere","signupFavor","lazyloadOakImages","mapleFreeTrial","adQuerySupportForMultipleUserAddedKeywords","removeSectionDependentAdLogicForJanuary","oakUpdateAdStride","autoPlaceNewsletter","piiBlockDFP","adExclusiveIERibbonCheck","FlexAdDoubleHeightTracking","didScroll","supportedByAdFullBleed","anchorsHttps","oakBylineMargin","TopFlexAdSiteWide","BundlePayFlow","loadArticleOptimizely","oakStyleTouchups","oakBasicHeader","serveOakOnVI","opinionHeadline","indexAsHeaderBidder","newsletterRegiTest","bookReviewBuyButton","caslOpt","sectionBasedAmp","abraInCustomParams","jkiddScript","blueKai","showNewDealbookLogo","commentsUserDataUpdateDisabled","tMagazineFontTest","disableServeAsNyt4","signupModuleTest"]</script>
-	<script>
-	var require = {
-		baseUrl: 'https://g1.nyt.com/assets/',
-		waitSeconds: 20,
-		paths: {
-			'foundation': 'article/20180322-142545/js/foundation',
-			'shared': 'article/20180322-142545/js/shared',
-			'article': 'article/20180322-142545/js/article',
-			'application': 'article/20180322-142545/js/article/story',
-			'videoFactory': 'https://static01.nyt.com/js2/build/video/2.0/videofactoryrequire',
-			'videoPlaylist': 'https://static01.nyt.com/js2/build/video/players/extended/2.0/appRequire',
-			'auth/mtr': 'https://static01.nyt.com/js/mtr',
-			'auth/growl': 'https://static01.nyt.com/js/auth/growl/default',
-			'vhs': 'https://static01.nyt.com/video/vhs/build/vhs-2.x.min',
-			'vhs3': 'https://static01.nyt.com/video-static/vhs3/vhs.min'
-		},
-		map: {
-			'*': {
-				'story/main': 'article/story/main'
-			}
-		}
-	};
-	</script>
-	<!--[if (gte IE 9)|!(IE)]> <!-->
-	<script data-main="foundation/main" src="https://g1.nyt.com/assets/article/20180322-142545/js/foundation/lib/framework.js"></script>
-	<!--<![endif]-->
-	<!--[if lt IE 9]>
-	<script>
-		require.map['*']['foundation/main'] = 'foundation/legacy_main';
-	</script>
-	<script data-main="foundation/legacy_main" src="https://g1.nyt.com/assets/article/20180322-142545/js/foundation/lib/framework.js"></script>
-	<![endif]-->
-		<script>
-		require(['foundation/main'], function () {
-			require(['auth/mtr', 'auth/growl']);
-		});
-		</script>
 	</head>
 	
 	<body>		 
+	<!-- Other Body -->
 	</body>
 	</html>
 
